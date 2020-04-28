43e99f8cddc5e621067a11a43569c9aee9561161
klosinm <monicaklosin@gmail.com>
2020-04-28 14:30:00 -0400

    problems 1 and 2 done/transported to github

---
+++ express.js
@@ -0,0 +1,44 @@
+const express = require('express');
+
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public")); //or single quotes
+app.set('view engine', 'ejs');
+
+app.engine('html', require('ejs').renderFile);
+
+
+/* Import the body-parser module.  (Used for parsing Post data) */
+const bodyParser = require('body-parser');
+
+
+//problem 1
+app.get('/getty', function (req, res) {
+    res.render('gettysburg.html');
+});
+
+//problem 2
+app.get('/maxMin', function (req, res) {
+    res.render('maxMin.html');
+});
+
+
+//problem 5
+app.get('/Order', function (req, res) {
+   
+});
+
+//problem4
+app.get('/list', function (req, res) {
+    
+});
+
+
+/* Parse the request body if there is POST data */
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+app.listen(port, () => console.log('On port!'))
